0df0f421d14344aca2403d7a7af712e7dad744c4
Viswaskena-J <97895225+Viswaksena-J@users.noreply.github.com>
2025-02-26 18:12:47 +0530

    Configure ESLint and Next.js build settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+}
+
 export default nextConfig;
